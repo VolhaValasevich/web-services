c642d64a8e368c652efa546113bb49e567c07b91
Volha Valasevich <volha_valasevich@epam.com>
2018-08-08 12:17:03 +0300

    add mocha to env

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         "es6": true,
         "node": true,
         "protractor": true,
-        "jasmine": true
+        "jasmine": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
