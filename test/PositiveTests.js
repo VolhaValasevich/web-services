c4d218bdce16379a16e30933b97539fbc8e79fd7
Volha Valasevich <volha_valasevich@epam.com>
2018-08-09 13:43:46 +0300

    combine test data

--- test/PositiveTests.js
+++ test/PositiveTests.js
@@ -6,20 +6,28 @@ const validate = require("../lib/validateSchema.js");
 const sections = require("../data/sections");
 const requestTypes = require("../data/requestTypes");
 const DELETE = 'Delete';
+const POST = "Post";
+const ID_MULTIPLIER = 5001; // the biggest section "Photos" has 5000 entries
 
-requestTypes.map((request) => {
+requestTypes.map((request) => { // runs a separate describe for each type of request
 
     describe(`${request.type} Tests`, () => {
-        sections.map((section) => {
-            const templates = require(`../data/${section.name}/${request.type}Positive${section.filename}`);
-            const schema = require(`../data/${section.name}/schema${section.filename}`);
+        sections.map((section) => { // runs tests for different data types (comments, posts, users)
+            const testData = require(`../data/${section.name}/Positive${section.filename}`); // requiring section test data
+            const schema = require(`../data/${section.name}/schema${section.filename}`); // requiring section json schema
 
-            templates.map((data) => {
+            testData.map((data) => { // runs tests for each element in test data
                 let response;
                 let id = parseInt(data.uri.split('/')[2], 10);
+                const uri = data.uri;
 
                 before(async () => {
-                    data.uri = env.uri + data.uri;
+                    data.uri = env.uri + uri;
+                    data.method = request.type.toUpperCase();
+                    if (request.type === POST) {
+                        data.uri = data.uri.slice(0, -1); // POST requests need to be sent to the common uri
+                        data.body.id *= ID_MULTIPLIER; // POST body data must have unique id
+                    }
                     response = await sendRequest(data);
                 });
 
