6c00430507259a631f94824c7aca6db5b1bbeeca
Volha Valasevich <volha_valasevich@epam.com>
2018-08-09 14:30:18 +0300

    add logger

--- test/PositiveTests.js
+++ test/PositiveTests.js
@@ -5,6 +5,7 @@ const env = require('../endpoint/test');
 const validate = require("../lib/validateSchema.js");
 const resources = require("../data/resources");
 const requestTypes = require("../data/requestTypes");
+const logger = require("../lib/logger.js").logger;
 const DELETE = 'Delete';
 const POST = "Post";
 const ID_MULTIPLIER = 5001; // the biggest section "Photos" has 5000 entries
@@ -13,6 +14,7 @@ requestTypes.map((request) => { // runs a separate describe for each type of req
 
     describe(`${request.type} Tests`, () => {
         resources.map((resource) => { // runs tests for different data types (comments, posts, users)
+            logger.info(`Running tests for ${resource.name}`);
             const testData = require(`../data/${resource.name}/Positive${resource.filename}`); // requiring resource test data
             const schema = require(`../data/${resource.name}/schema${resource.filename}`); // requiring resource json schema
 
@@ -28,27 +30,33 @@ requestTypes.map((request) => { // runs a separate describe for each type of req
                         data.uri = data.uri.slice(0, -1); // POST requests need to be sent to the common uri
                         data.body.id *= ID_MULTIPLIER; // POST body data must have unique id
                     }
+                    logger.action('Sending request to ' + data.uri);
                     response = await sendRequest(data);
                 });
 
                 it(`Check response code of ${resource.singular} ` + id, () => {
+                    logger.check(`Checking response code of ${resource.singular} ` + id);
                     expect(response.statusCode).to.eql(request.codeSuccess);
                 });
 
                 it(`Check response message of ${resource.singular} ` + id, () => {
+                    logger.check(`Checking response message of ${resource.singular} ` + id);
                     expect(response.statusMessage).to.eql(request.messageSuccess);
                 });
 
-                it(`Validate response body of ${resource.singular} ` + id, () => {
+                it(`Check response body of ${resource.singular} ` + id, () => {
+                    logger.check(`Checking response body of ${resource.singular} ` + id);
                     if (request.type === DELETE) {
                         expect(response.body).to.eql({});
                     } else {
+                        logger.action('Validating response JSON schema');
                         expect(validate(response.body, schema)).to.eql(true);
                     }
                 });
 
                 if (request.bodyIsRequired) {
                     it(`Compare recieved data with sent data in ${resource.singular} ` + id, () => {
+                        logger.check(`Comparing recieved data with sent data in ${resource.singular} ` + id);
                         expect(response.body).to.eql(data.body);
                     });
                 }
