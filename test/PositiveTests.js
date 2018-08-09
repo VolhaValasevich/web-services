6707635b1dfa2fbac35a2a24d61ca4df9b52f99b
Volha Valasevich <volha_valasevich@epam.com>
2018-08-09 13:51:37 +0300

    rename resources

--- test/PositiveTests.js
+++ test/PositiveTests.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const sendRequest = require('../lib/sendRequest');
 const env = require('../endpoint/test');
 const validate = require("../lib/validateSchema.js");
-const sections = require("../data/sections");
+const resources = require("../data/resources");
 const requestTypes = require("../data/requestTypes");
 const DELETE = 'Delete';
 const POST = "Post";
@@ -12,9 +12,9 @@ const ID_MULTIPLIER = 5001; // the biggest section "Photos" has 5000 entries
 requestTypes.map((request) => { // runs a separate describe for each type of request
 
     describe(`${request.type} Tests`, () => {
-        sections.map((section) => { // runs tests for different data types (comments, posts, users)
-            const testData = require(`../data/${section.name}/Positive${section.filename}`); // requiring section test data
-            const schema = require(`../data/${section.name}/schema${section.filename}`); // requiring section json schema
+        resources.map((resource) => { // runs tests for different data types (comments, posts, users)
+            const testData = require(`../data/${resource.name}/Positive${resource.filename}`); // requiring resource test data
+            const schema = require(`../data/${resource.name}/schema${resource.filename}`); // requiring resource json schema
 
             testData.map((data) => { // runs tests for each element in test data
                 let response;
@@ -31,11 +31,11 @@ requestTypes.map((request) => { // runs a separate describe for each type of req
                     response = await sendRequest(data);
                 });
 
-                it(`Check response code of ${section.singular} ` + id, () => {
+                it(`Check response code of ${resource.singular} ` + id, () => {
                     expect(response.statusCode).to.eql(request.codeSuccess);
                 });
 
-                it(`Validate response body of ${section.singular} ` + id, () => {
+                it(`Validate response body of ${resource.singular} ` + id, () => {
                     if (request.type === DELETE) {
                         expect(response.body).to.eql({});
                     } else {
@@ -44,7 +44,7 @@ requestTypes.map((request) => { // runs a separate describe for each type of req
                 });
 
                 if (request.bodyRequired) {
-                    it(`Compare recieved data with sent data in ${section.singular} ` + id, () => {
+                    it(`Compare recieved data with sent data in ${resource.singular} ` + id, () => {
                         expect(response.body).to.eql(data.body);
                     });
                 }
