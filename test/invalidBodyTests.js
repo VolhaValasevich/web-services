cde2b79f96403d789556bc2e83ad27da3407ea59
Volha Valasevich <volha_valasevich@epam.com>
2018-08-10 14:50:57 +0300

    add invalid body tests

---
+++ test/invalidBodyTests.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sendRequest = require('../lib/sendRequest');
+const env = require('../endpoint/test');
+const codes = require("../data/statusCodes");
+const resources = require("../data/resources");
+const method = "POST";
+
+describe('Incorrect Body Type Tests', () => {
+    resources.forEach((resource) => {
+        describe(resource.filename, () => {
+            const invalidData = require("../data/negative/invalid" + resource.filename);
+            const uri = env.uri + resource.uri;
+            let response;
+            invalidData.forEach((data) => {
+
+                before(async () => {
+                    response = await sendRequest(uri, method, data.body);
+                });
+
+                it(`should return correct error code after sending body with ${data.info} to ${uri}`, async () => {
+                    expect(response.statusCode).to.eql(codes.internalServerError.code);
+                });
+
+                it(`should return correct error message after sending body with ${data.info} to ${uri}`, async () => {
+                    expect(response.statusMessage).to.eql(codes.internalServerError.message);
+                });
+
+            });
+        });
+
+    });
+});
